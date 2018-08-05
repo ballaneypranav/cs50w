04da230029db187a1558428b76828860d8d39421
Pranav <ballaneypranav@gmail.com>
2018-08-05 16:13:08 +0530

    starting over

--- first/application.py
+++ first/application.py
@@ -1,12 +1,8 @@
-from flask import Flask, render_template, request
+from flask import Flask
+
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
-
-@app.route("/hello", methods=["POST"])
-def hello():
-    name = request.form.get("name")
-    return render_template("hello.html", name=name)
+	return "Hello, world!"
